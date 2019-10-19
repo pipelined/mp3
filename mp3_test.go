4285048af40d338cb95474fb84e1eab2935130eb
Sergei Dudka <dudk@uber.com>
2019-10-19 17:38:23 +0200

    new-api: remove unused variables

--- mp3_test.go
+++ mp3_test.go
@@ -30,64 +30,64 @@ func TestMp3(t *testing.T) {
 			channelMode: mp3.JointStereo,
 			vbr:         mp3.CBR(320),
 		},
-		// {
-		// 	inFile:      sample,
-		// 	channelMode: mp3.JointStereo,
-		// 	vbr:         mp3.CBR(192),
-		// },
-		// {
-		// 	inFile:      sample,
-		// 	channelMode: mp3.JointStereo,
-		// 	vbr:         mp3.ABR(220),
-		// },
-		// {
-		// 	inFile:      sample,
-		// 	channelMode: mp3.JointStereo,
-		// 	vbr:         mp3.ABR(128),
-		// },
-		// {
-		// 	inFile:      sample,
-		// 	channelMode: mp3.JointStereo,
-		// 	vbr:         mp3.VBR(0),
-		// },
-		// {
-		// 	inFile:      sample,
-		// 	channelMode: mp3.JointStereo,
-		// 	vbr:         mp3.VBR(9),
-		// },
-		// {
-		// 	inFile:      sample,
-		// 	channelMode: mp3.Mono,
-		// 	vbr:         mp3.VBR(9),
-		// },
-		// {
-		// 	inFile:      sample,
-		// 	channelMode: mp3.Mono,
-		// 	vbr:         mp3.VBR(9),
-		// 	useQuality:  true,
-		// 	quality:     9,
-		// },
-		// {
-		// 	inFile:      sample,
-		// 	channelMode: mp3.JointStereo,
-		// 	vbr:         mp3.VBR(0),
-		// 	useQuality:  true,
-		// 	quality:     0,
-		// },
-		// {
-		// 	inFile:      sample,
-		// 	channelMode: mp3.JointStereo,
-		// 	vbr:         mp3.VBR(0),
-		// 	useQuality:  true,
-		// 	quality:     9,
-		// },
-		// {
-		// 	inFile:      sample,
-		// 	channelMode: mp3.Stereo,
-		// 	vbr:         mp3.VBR(0),
-		// 	useQuality:  true,
-		// 	quality:     3,
-		// },
+		{
+			inFile:      sample,
+			channelMode: mp3.JointStereo,
+			vbr:         mp3.CBR(192),
+		},
+		{
+			inFile:      sample,
+			channelMode: mp3.JointStereo,
+			vbr:         mp3.ABR(220),
+		},
+		{
+			inFile:      sample,
+			channelMode: mp3.JointStereo,
+			vbr:         mp3.ABR(128),
+		},
+		{
+			inFile:      sample,
+			channelMode: mp3.JointStereo,
+			vbr:         mp3.VBR(0),
+		},
+		{
+			inFile:      sample,
+			channelMode: mp3.JointStereo,
+			vbr:         mp3.VBR(9),
+		},
+		{
+			inFile:      sample,
+			channelMode: mp3.Mono,
+			vbr:         mp3.VBR(9),
+		},
+		{
+			inFile:      sample,
+			channelMode: mp3.Mono,
+			vbr:         mp3.VBR(9),
+			useQuality:  true,
+			quality:     9,
+		},
+		{
+			inFile:      sample,
+			channelMode: mp3.JointStereo,
+			vbr:         mp3.VBR(0),
+			useQuality:  true,
+			quality:     0,
+		},
+		{
+			inFile:      sample,
+			channelMode: mp3.JointStereo,
+			vbr:         mp3.VBR(0),
+			useQuality:  true,
+			quality:     9,
+		},
+		{
+			inFile:      sample,
+			channelMode: mp3.Stereo,
+			vbr:         mp3.VBR(0),
+			useQuality:  true,
+			quality:     3,
+		},
 	}
 
 	for i, test := range tests {
@@ -96,7 +96,7 @@ func TestMp3(t *testing.T) {
 		assert.Nil(t, err)
 		pump := mp3.Pump{Reader: inFile}
 
-		outFile, err := os.Create(fmt.Sprintf("%s_%d_%s.mp3", out, i, test.vbr))
+		outFile, err := os.Create(fmt.Sprintf("%s-%d-%s.mp3", out, i, test.vbr))
 		assert.Nil(t, err)
 		sink := &mp3.Sink{
 			Writer:      outFile,
